cb5d52d790bdc6091f71bf6e9f45e3e76cbe632c
Vinicius <vsslviniciussousa@gmail.com>
2024-11-29 22:16:53 -0400

    Primeiro commit

---
+++ 05-FirstStepsInJavaWithSpringBoot/first-steps/src/test/java/br/com/vssl/first_steps/StartupTest.java
@@ -0,0 +1,13 @@
+package br.com.vssl.first_steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
